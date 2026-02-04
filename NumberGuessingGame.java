8848898a2932813a3d2c8177801c005e33384f45
divyasmurti-cmd <patrodivyasmurti@gmail.com>
2026-02-04 17:05:50 +0530

    Add files via upload
    Completed Task 02 – Number Guessing Game

---
+++ NumberGuessingGame.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuessingGame {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	Random random=new Random();
+	int noToGuess=random.nextInt(100)+1;
+	int userGuess=0;
+	int attempts=0;
+	System.out.println("Welcome to the Number Guessing Game!");
+    System.out.println("Guess a number between 1 and 100");
+    while(userGuess!=noToGuess) {
+    	 System.out.print("Enter your guess: ");
+    	 userGuess=sc.nextInt();
+    	 attempts++;
+    	 if(userGuess>noToGuess) {
+    		 System.out.println("Too high! Try again.");
+    		 
+    	 }
+    	 else if (userGuess < noToGuess) {
+             System.out.println("Too low! Try again.");
+         } else {
+             System.out.println(" Congratulations! You guessed the number.");
+             System.out.println("Total attempts: " + attempts);
+         }
+    }
+  
+
+	}
+
+}
